38d18d281885b67fe748e5dd7c42db95d0058120
caminobelllo <alstpgml0812@gmail.com>
2023-08-25 23:26:41 +0900

    feat: 어제기록 페이지 레이아웃 구현

--- myapp/src/App.js
+++ myapp/src/App.js
@@ -3,6 +3,7 @@ import { Container } from "./Containter";
 import Header from "./components/Header";
 import Logo from "./components/Logo";
 import TodayPage from "./pages/TodayPage";
+import PrevPage from "./pages/PrevPage";
 
 function App() {
   return (
@@ -10,7 +11,8 @@ function App() {
       <Container>
         <Logo />
         {/* <Header text="로그인 하기" /> */}
-        <TodayPage />
+        {/* <TodayPage /> */}
+        <PrevPage />
       </Container>
     </div>
   );
